d2ca3830446f515c3f19033c67d9b7fb75d3953f
TonySilva7 <tony.deve7oper@gmail.com>
2024-01-29 11:53:56 -0300

    chore: add and config OneSignal

--- App.tsx
+++ App.tsx
@@ -7,7 +7,13 @@ import { Routes } from './src/routes';
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
+import { OneSignal } from 'react-native-onesignal';
+
+
+
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("393332da-1e78-4a81-866b-e753a8c60aa7");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
