a174105e6d23beccd5517ef3d4fe218b0a6b94d4
TonySilva7 <tony.deve7oper@gmail.com>
2024-01-31 11:45:53 -0300

    feat: implement sdk one signal

--- src/libs/onesignal.ts
+++ src/libs/onesignal.ts
@@ -2,17 +2,7 @@ import { OneSignal } from 'react-native-onesignal';
 import { Platform } from 'react-native';
 
 function oneSignalInitialize() {
-  // const {
-  //   EXPO_PUBLIC_ONE_SIGNAL_APP_ID_ANDROID,
-  //   EXPO_PUBLIC_ONE_SIGNAL_APP_ID_IOS,
-  // } = process.env;
-
-  const oneSignalAppId =
-    // Platform.OS === 'ios'
-    //   ? EXPO_PUBLIC_ONE_SIGNAL_APP_ID_IOS
-    //   : EXPO_PUBLIC_ONE_SIGNAL_APP_ID_ANDROID;
-
-    Platform.OS === 'ios'
+  const oneSignalAppId = Platform.OS === 'ios'
       ? 'empty'
       : '393332da-1e78-4a81-866b-e753a8c60aa7'
 
@@ -25,7 +15,8 @@ function oneSignalInitialize() {
   }
 
   if (Platform.OS === 'ios') {
-    OneSignal.Notifications.canRequestPermission().then((response) => {
+    OneSignal.Notifications.canRequestPermission()
+    .then((response) => {
       if (response) {
         OneSignal.Notifications.requestPermission(true);
       }
