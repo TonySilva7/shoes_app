e3c3679913cf444e8641cb01f4a55db390833907
TonySilva7 <tony.deve7oper@gmail.com>
2024-02-06 13:57:13 -0300

    feat: implments deep link

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,22 @@
 import { useTheme } from 'native-base';
-import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { DefaultTheme, LinkingOptions, NavigationContainer } from '@react-navigation/native';
 
 import { AppRoutes } from './app.routes';
+
+const linking: LinkingOptions<ReactNavigation.RootParamList> | undefined = {
+  prefixes : ["igniteshoesapp://", "com.devlife.shoesapp://", "exp+igniteshoesapp://"],
+  config: {
+    screens: {
+
+      details: {
+        path: 'details/:productId',
+        parse: {
+          productId: (productId: string) => productId
+        }
+      }
+    }
+  }
+}
 
 export function Routes() {
   const { colors } = useTheme();
@@ -10,7 +25,7 @@ export function Routes() {
   theme.colors.background = colors.gray[700];
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
     </NavigationContainer>
   );
